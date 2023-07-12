26fb049f7f1ef1153cb59575f4836be29f59f268
Fede Krenn <fedekrenn@gmail.com>
2023-07-12 16:49:25 -0300

    Se termina la lógica del trabajo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+type Producto = {
+  nombre: string;
+  costo: number;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'trabajoFinalAngular';
+  
+  listaProductos: Producto[] = [
+    { nombre: 'iPhone 12', costo: 1000 },
+    { nombre: 'PS5', costo: 500 },
+    { nombre: 'MacBook Pro', costo: 2000 },
+    { nombre: 'Xbox Series X', costo: 500 },
+  ];
+
+  mostrarLista: boolean = true;
+
+  toggleLista() {
+    this.mostrarLista = !this.mostrarLista;
+  }
 }
